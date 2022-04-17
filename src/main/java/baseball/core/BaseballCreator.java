61cff2e9c8f3efc91de527ad1d97499ee714cc2f
dhslrl321 <dhslrl321@gmail.com>
2022-04-18 00:50:45 +0900

    fix : Baseball 이 유일한 숫자로 반환할 수 있도록 변경한다

--- src/main/java/baseball/core/BaseballCreator.java
+++ src/main/java/baseball/core/BaseballCreator.java
@@ -2,12 +2,28 @@ package baseball.core;
 
 import baseball.domain.Baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballCreator {
 
     public Baseball create() {
-        List<Integer> randoms = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        Set<Integer> uniqueNumberSet = pickUniqueNumberSet();
+        List<Integer> randoms = setToList(uniqueNumberSet);
         return Baseball.from(randoms);
+    }
+
+    private List<Integer> setToList(Set<Integer> set) {
+        return new ArrayList<>(set);
+    }
+
+    private Set<Integer> pickUniqueNumberSet() {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while (uniqueNumbers.size() < 3) {
+            uniqueNumbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return uniqueNumbers;
     }
 }
